f0bf6c6e60551f2ac31cf91338fc35a3613e3c0b
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-11-16 12:25:28 -0600

    Count entries for Title ID removed

--- .ci/remove-ids.py
+++ .ci/remove-ids.py
@@ -4,6 +4,7 @@ import os
 import json
 
 if __name__ == '__main__':
+  entries = 0
   for patches in glob.glob('output/json/*.json', recursive=True):
     with open(patches, 'r') as fr:
       cont = json.load(fr)
@@ -11,5 +12,7 @@ if __name__ == '__main__':
       length_id = len(json_id)
       for i in range(0, length_id):
         del json_id[i]['app_titleid']
+        entries += 1
       with open(patches, 'w') as fw:
         fw.write(f'{json.dumps(cont, indent=2)}\n')
+  print('processed %i entries' % (entries))
