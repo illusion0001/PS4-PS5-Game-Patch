81beab6485308af95f3c344f21dbaf577935c09b
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2022-12-14 06:58:38 -0600

    CI Test Unit: defines constants

--- .ci/example-01.c
+++ .ci/example-01.c
@@ -7,6 +7,21 @@
 #define MAX_TOKENS 10240 // should be plenty
 #define debug_printf(a, args...) printf("[%s] (%s:%d) " a, __func__, __FILE__, __LINE__, ##args)
 
+#define KEY_PATCH "patch"
+#define KEY_TITLE "title"
+#define KEY_APP_VER "app_ver"
+#define KEY_PATCH_VER "patch_ver"
+#define KEY_NAME "name"
+#define KEY_AUTHOR "author"
+#define KEY_NOTE "note"
+#define KEY_APP_TITLEID "app_titleid"
+#define KEY_APP_ELF "app_elf"
+#define KEY_APP_TYPE "type"
+#define KEY_APP_ADDR "addr"
+#define KEY_APP_VALUE "value"
+#define KEY_PATCH_LIST "patch_list"
+#define KEY_COMMENT "comment"
+
 int main(int argc, char *argv[]) {
     if (argc == 2) {
         char *str = 0;
@@ -32,9 +47,9 @@ int main(int argc, char *argv[]) {
         }
 
         debug_printf("read: %s size: %lu bytes\n", argv[1], length);
-        json_t const *patchItems = json_getProperty(json, "patch");
+        json_t const *patchItems = json_getProperty(json, KEY_PATCH);
         if (!patchItems || JSON_ARRAY != json_getType(patchItems)) {
-            printf("Error, the patches list property is not found\n");
+            printf("Error, the %s list property is not found\n", KEY_PATCH);
             return 1;
         }
 
@@ -42,48 +57,48 @@ int main(int argc, char *argv[]) {
         for (patches = json_getChild(patchItems); patches != 0;
              patches = json_getSibling(patches)) {
             if (JSON_OBJ == json_getType(patches)) {
-                char const *gameTitle = json_getPropertyValue(patches, "title");
+                char const *gameTitle = json_getPropertyValue(patches, KEY_TITLE);
                 if (gameTitle)
-                    debug_printf("title: %s\n", gameTitle);
+                    debug_printf("%s: %s\n", KEY_TITLE, gameTitle);
                 else
-                    debug_printf("title: not found\n");
-                char const *gameAppver = json_getPropertyValue(patches, "app_ver");
+                    debug_printf("%s: not found\n", KEY_TITLE);
+                char const *gameAppver = json_getPropertyValue(patches, KEY_APP_VER);
                 if (gameAppver)
-                    debug_printf("app_ver: %s\n", gameAppver);
+                    debug_printf("%s: %s\n", KEY_APP_VER, gameAppver);
                 else
-                    debug_printf("app_ver: not found\n");
-                char const *gameName = json_getPropertyValue(patches, "name");
+                    debug_printf("%s: not found\n", KEY_APP_VER);
+                char const *gameName = json_getPropertyValue(patches, KEY_NAME);
                 if (gameName)
-                    debug_printf("name: %s\n", gameName);
+                    debug_printf("%s: %s\n", KEY_NAME, gameName);
                 else
-                    debug_printf("name: not found\n");
-                char const *gameAuthor = json_getPropertyValue(patches, "author");
+                    debug_printf("%s: not found\n", KEY_NAME);
+                char const *gameAuthor = json_getPropertyValue(patches, KEY_AUTHOR);
                 if (gameAuthor)
-                    debug_printf("author: %s\n", gameAuthor);
+                    debug_printf("%s: %s\n", KEY_AUTHOR, gameAuthor);
                 else
-                    debug_printf("author: not found\n");
-                char const *gameNote = json_getPropertyValue(patches, "note");
+                    debug_printf("%s: not found\n", KEY_AUTHOR);
+                char const *gameNote = json_getPropertyValue(patches, KEY_NOTE);
                 if (gameNote)
-                    debug_printf("note: %s\n", gameNote);
+                    debug_printf("%s: %s\n", KEY_NOTE, gameNote);
                 else
-                    debug_printf("note: not found\n");
-                json_t const *patch_List_Items = json_getProperty(patches, "patch_list");
+                    debug_printf("%s: not found\n", KEY_NOTE);
+                json_t const *patch_List_Items = json_getProperty(patches, KEY_PATCH_LIST);
                 json_t const *patch_lists;
                 for (patch_lists = json_getChild(patch_List_Items); patch_lists != 0;
                      patch_lists = json_getSibling(patch_lists)) {
                     if (JSON_OBJ == json_getType(patch_lists)) {
-                        char const *gameType = json_getPropertyValue(patch_lists, "type");
+                        char const *gameType = json_getPropertyValue(patch_lists, KEY_APP_TYPE);
                         if (gameType)
-                            debug_printf("  type: %s\n", gameType);
-                        char const *gameAddr = json_getPropertyValue(patch_lists, "addr");
+                            debug_printf("  %s: %s\n", KEY_APP_TYPE, gameType);
+                        char const *gameAddr = json_getPropertyValue(patch_lists, KEY_APP_ADDR);
                         if (gameAddr)
-                            debug_printf("  addr: %s\n", gameAddr);
-                        char const *gameValue = json_getPropertyValue(patch_lists, "value");
+                            debug_printf("  %s: %s\n", KEY_APP_ADDR, gameAddr);
+                        char const *gameValue = json_getPropertyValue(patch_lists, KEY_APP_VALUE);
                         if (gameValue)
-                            debug_printf("  val: %s\n", gameValue);
-                        char const *gameComm = json_getPropertyValue(patch_lists, "comment");
+                            debug_printf("  %s: %s\n", KEY_APP_VALUE, gameValue);
+                        char const *gameComm = json_getPropertyValue(patch_lists, KEY_COMMENT);
                         if (gameComm)
-                            debug_printf("  comment: %s\n", gameComm);
+                            debug_printf("  %s: %s\n", KEY_COMMENT, gameComm);
                     }
                 }
             }
