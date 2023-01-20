13acfc05c13f452d85d8e022fa7213665dd5c6ce
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-01-20 00:35:28 -0600

    Add files via upload

---
+++ .ci/file-autogen-json.py
@@ -0,0 +1,17 @@
+import glob
+from pathlib import Path
+import yaml
+import os
+import json
+
+if __name__ == '__main__':
+  for patches in glob.glob('patches_old/json/*.json', recursive=True):
+    app_titleid_str = 'app_titleid'
+    with open(patches, 'r') as file:
+      patch_file = json.load(file)
+      json_id = patch_file["patch"]
+      patch_file['base_file'] = patches
+      for i in range(0, len(json_id[0][app_titleid_str])):
+        out = ('output/json/{}.json'.format(json_id[0][app_titleid_str][i]))
+        with open(out, 'w') as fw:
+          fw.write(json.dumps(patch_file, indent=2))
