dc8b057b607250f815852d58b8f6b0379e71c31e
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-02-08 20:14:31 -0600

    Update file-autogen-xml.py

--- .ci/file-autogen-xml.py
+++ .ci/file-autogen-xml.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
             print('Source: %s' % (patches))
             with open(patches, 'r') as fr:
                 string_data = fr.read()
+                string_data = string_data.replace('<?xml version="1.0" encoding="utf-8"?>\n', f'<?xml version="1.0" encoding="utf-8"?>\n<!-- File generated from: {patches} -->\n')
                 root = ET.fromstring(string_data)
                 ID = root.findall('TitleID/ID')
                 for i in ID:
                     out = ('output/xml/{}.xml'.format(i.text))
                     print('Output: %s' % (out))
                     with open(out, 'w') as fw:
-                        string_data = string_data.replace('<?xml version="1.0" encoding="utf-8"?>\n', f'<?xml version="1.0" encoding="utf-8"?>\n<!-- File generated from: {patches} -->\n')
                         fw.write(string_data)
                     entry = entry + 1
         print('processed %u entries' % (entry))
