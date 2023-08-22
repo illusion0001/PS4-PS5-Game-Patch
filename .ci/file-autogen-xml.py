f05ffcc48313ecdcbd3125baf3f25b69a1dd3804
illusion0001 <37698908+illusion0001@users.noreply.github.com>
2023-08-21 22:32:02 -0500

    CI: Add build date to files (#76)

--- .ci/file-autogen-xml.py
+++ .ci/file-autogen-xml.py
@@ -5,21 +5,26 @@ import xml.etree.ElementTree as ET
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 3:
         entry = 0
         for patches in glob.glob(sys.argv[1], recursive=True):
-            print('Source: %s' % (patches))
+            # print('Source: %s' % (patches))
             with open(patches, 'r') as fr:
                 string_data = fr.read()
-                string_data = string_data.replace('<?xml version="1.0" encoding="utf-8"?>\n', f'<?xml version="1.0" encoding="utf-8"?>\n<!-- File generated from: {patches} -->\n')
+                string_data = string_data.replace('<?xml version="1.0" encoding="utf-8"?>\n', f'<?xml version="1.0" encoding="utf-8"?>\n<!-- File generated from: {patches} @ {sys.argv[2]} -->\n')
                 root = ET.fromstring(string_data)
                 ID = root.findall('TitleID/ID')
                 for i in ID:
+                    if not (i.text.startswith('CUSA') or i.text.startswith('PPSA')):
+                        print(f'Ignored {i.text}')
+                        continue
                     out = ('output/xml/{}.xml'.format(i.text))
-                    print('Output: %s' % (out))
+                    # print('Output: %s' % (out))
                     with open(out, 'w') as fw:
                         fw.write(string_data)
                     entry = entry + 1
         print('processed %u entries' % (entry))
+        exit(0)
     else:
-        print('path to glob not provided')
+        print('path to glob not or current time provided')
+        exit(1)
